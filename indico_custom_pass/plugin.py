9bf6944153aa40d3a9067b02579c59c9067c2579
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-09-19 04:26:44 +0800

    Add support for custom logos and icons in Apple Wallet passes

--- indico_custom_pass/plugin.py
+++ indico_custom_pass/plugin.py
@@ -5,6 +5,7 @@ from indico_patcher import patch
 
 from indico_custom_pass.settings import CustomPassSettingsForm
 
+
 class CustomPassPlugin(IndicoPlugin):
     """Custom Pass
 
@@ -16,7 +17,10 @@ class CustomPassPlugin(IndicoPlugin):
         'apple_pass_background_color': '#007cac',
         'apple_pass_foreground_color': '#ffffff',
         'apple_pass_label_color': '#ffffff',
-        'apple_pass_organization_name': 'Indico',
+        'apple_pass_logo_text': 'Indico',
+        'apple_pass_customize_logo': False,
+        'apple_pass_custom_icon_url': '',
+        'apple_pass_custom_logo_url': '',
         'google_pass_background_color': '#007cac'
     }
 
@@ -31,7 +35,21 @@ class AppleWalletManagerMixin(AppleWalletManager):
         p.backgroundColor = CustomPassPlugin.settings.get('apple_pass_background_color')
         p.foregroundColor = CustomPassPlugin.settings.get('apple_pass_foreground_color')
         p.labelColor = CustomPassPlugin.settings.get('apple_pass_label_color')
-        p.organizationName = CustomPassPlugin.settings.get('apple_pass_organization_name')
+        p.logoText = CustomPassPlugin.settings.get('apple_pass_logo_text')
+        if bool(CustomPassPlugin.settings.get('apple_pass_customize_logo')):
+            # Per apple docs:
+            # The dimensions given above are all in points. On a non-Retina display, each point equals exactly 1 pixel.
+            # On a Retina display, there are 2 or 3 pixels per point, depending on the device.
+            # To support all screen sizes and resolutions, provide the original, @2x, and @3x versions of your art.
+            # TODO
+            # This is a easy way to fool apple to render the logo at higher resolutions, however the user shall have the
+            # choice to provide logos for different aspect ratios, to allow for more efficient ticket sizes
+            p.add_file_from_url('icon.png', CustomPassPlugin.settings.get('apple_pass_custom_icon_url'))
+            p.add_file_from_url('icon@2x.png', CustomPassPlugin.settings.get('apple_pass_custom_icon_url'))
+            p.add_file_from_url('icon@3x.png', CustomPassPlugin.settings.get('apple_pass_custom_icon_url'))
+            p.add_file_from_url('logo.png', CustomPassPlugin.settings.get('apple_pass_custom_logo_url'))
+            p.add_file_from_url('logo@2x.png', CustomPassPlugin.settings.get('apple_pass_custom_logo_url'))
+            p.add_file_from_url('logo@3x.png', CustomPassPlugin.settings.get('apple_pass_custom_logo_url'))
         return p
 
 
